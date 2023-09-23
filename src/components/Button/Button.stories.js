364577d68a364f0da8bff3d246234728d23115b0
shubh <shubham998845@gmail.com>
2023-09-23 23:37:32 +0530

    some changes

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,6 +4,9 @@ import Button from "./Button";
 export default {
   title: "Form/Button",
   component: Button,
+  args: {
+    children: "Button", //will get overriden by below if same given below
+  },
 };
 
 export const Primary = () => <Button variant="primary">Primary</Button>;
@@ -14,18 +17,22 @@ export const Success = () => <Button variant="success">Success</Button>;
 
 export const Danger = () => <Button variant="danger">Danger</Button>;
 
-// tenplate aproach is much better than above
+// tenplate aproach is much better than above in complex jsx codes
 const Template = (args) => <Button {...args} />;
 
 export const PrimaryA = Template.bind({});
-
 PrimaryA.args = {
   variant: "primary",
   children: "Primary Args",
 };
 
-export const SecondaryA = Template.bind({});
+export const LongPrimaryA = Template.bind({});
+LongPrimaryA.args = {
+  ...PrimaryA.args,
+  children: "Long Primary Args",
+};
 
+export const SecondaryA = Template.bind({});
 SecondaryA.args = {
   variant: "secondary",
   children: "Secondary Args",
